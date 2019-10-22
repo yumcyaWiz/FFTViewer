edf83d34a7b310251388cf131ca385ab08ff1bbe
yumcyawiz <hashdolf1923@gmail.com>
2019-10-23 02:21:18 +0900

    include fftw

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 #include "glad/glad.h"
-#include <GLFW/glfw3.h>
+#include "GLFW/glfw3.h"
 
 #include "imgui.h"
 #include "examples/imgui_impl_glfw.h"
 #include "examples/imgui_impl_opengl3.h"
+
+#include "fftw3.h"
 
 
 int main() {
